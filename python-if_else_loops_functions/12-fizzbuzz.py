64da401346e8c0fd6ff62b158733835f8dc2caa2
charlene <charlenee.huynh@gmail.com>
2026-04-23 16:53:56 +0000

    12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@
 
 def fizzbuzz():
     for i in range(1, 101):
-        if i % 15 == 0:
+        if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
         elif i % 5 == 0:
             print("Buzz", end=" ")
@@ -11,4 +11,3 @@ def fizzbuzz():
             print("Fizz", end=" ")
         else:
             print("{}".format(i), end=" ")
-    print("")
