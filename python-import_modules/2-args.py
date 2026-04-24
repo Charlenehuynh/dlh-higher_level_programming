19795088d360da1f2808e70a0ec07ba9c795e8c5
Charlene <charlenee.huynh@gmail.com>
2026-04-24 17:21:44 +0200

    2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    count = len(args)
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print(f"{count} arguments:")
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
