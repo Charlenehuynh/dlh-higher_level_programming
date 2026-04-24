936a10cb39f42e092c813227026bb237f43a4424
Charlene <charlenee.huynh@gmail.com>
2026-04-24 17:22:59 +0200

    2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     else:
         print(f"{count} arguments:")
     for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{}: {}".format(i+1, sys.argv[i+1]))
