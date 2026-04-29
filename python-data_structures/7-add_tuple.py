7265a0f86e59d44488970690d1cf377b1e21a1ef
Charlene <charlenee.huynh@gmail.com>
2026-04-29 16:57:44 +0200

    7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def add_tuple(tuple_a=(), tuple_b=()):
     a1 = tuple_a[0] if len(tuple_a) >= 1 else 0
     a2 = tuple_a[1] if len(tuple_a) >= 2 else 0
@@ -9,12 +8,3 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     new_tuple = (a1 + b1, a2 + b2)
     return new_tuple
-
-
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
-
-print(add_tuple(tuple_a, (1,)))
-print(add_tuple(tuple_a, ()))
