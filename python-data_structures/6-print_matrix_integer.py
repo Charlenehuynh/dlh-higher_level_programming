d49dbac447d8a1c77c4360df5a296a6dda4c006c
Charlene <charlenee.huynh@gmail.com>
2026-04-29 15:42:28 +0200

    6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    if matrix == None:
+        return
+    for row in matrix:
+        for i in row:
+            print(i, end=" ")
+        print("")
+
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
