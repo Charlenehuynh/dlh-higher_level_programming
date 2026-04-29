b9bb8816d40f6b844217d986d8740a2e689d2c6a
Charlene <charlenee.huynh@gmail.com>
2026-04-29 17:19:36 +0200

    9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    current_max = my_list[0]
+    for i in my_list:
+        if i > current_max:
+            current_max = i
+    return current_max
+
+my_list = [1, 90, 2, 13, 34, 5, -13, 3]
+max_value = max_integer(my_list)
+print("Max: {}".format(max_value))
