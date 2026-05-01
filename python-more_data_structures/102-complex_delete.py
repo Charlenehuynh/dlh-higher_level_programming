7bf6ab9c4008785acf06017024b8572659367571
Charlene <charlenee.huynh@gmail.com>
2026-05-01 09:44:10 +0200

    102

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -2,7 +2,7 @@
 
 
 def complex_delete(a_dictionary, value):
-    for k,v in list(a_dictionary.items()):
+    for k, v in list(a_dictionary.items()):
         if v == value:
             a_dictionary.pop(k)
     return a_dictionary
