35633ce594f247d2f595f0828959c01270ccd25c
Charlene <charlenee.huynh@gmail.com>
2026-05-02 09:14:57 +0200

    0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -10,7 +10,7 @@ def safe_print_list(my_list=[], x=0):
     except IndexError:
         pass
     finally:
-        print("")
+        print()
     return count
 
 
--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -2,4 +2,8 @@
 
 
 def safe_print_integer_err(value):
-    
+    try:
+        print("{:d}".format(value))
+    except (TypeError, ValueError):
+        
+
