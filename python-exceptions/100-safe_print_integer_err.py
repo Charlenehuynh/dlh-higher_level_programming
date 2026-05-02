719b8452ed01e0b7bd2147c966367d18679abb0d
Charlene <charlenee.huynh@gmail.com>
2026-05-02 09:25:01 +0200

    100

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
@@ -9,7 +10,8 @@ def safe_print_integer_err(value):
         print("Exception: {}".format(e), file=sys.stderr)
         return False
 
-'''
+
+"""
 value = 89
 has_been_print = safe_print_integer_err(value)
 if not has_been_print:
@@ -24,4 +26,4 @@ value = "School"
 has_been_print = safe_print_integer_err(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
-'''
+"""
