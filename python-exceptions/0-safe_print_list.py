8f613c8c5059a866b2a954b4f14279bbc056c051
Charlene <charlenee.huynh@gmail.com>
2026-05-02 09:00:05 +0200

    0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,14 +7,13 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
             count += 1
-    except (IndexError, TypeError):
+    except (IndexError):
         pass
     finally:
         print("")
     return count
 
 
-"""
 
 my_list = [1, 2, 3, 4, 5]
 
@@ -24,4 +23,3 @@ nb_print = safe_print_list(my_list, len(my_list))
 print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list(my_list, len(my_list) + 2)
 print("nb_print: {:d}".format(nb_print))
-"""
