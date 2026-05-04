5937c1ee37f7a333c874c383c716dfe6314755ea
Charlene <charlenee.huynh@gmail.com>
2026-05-04 11:37:19 +0200

    1

---
+++ python-classes/1-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module defines a Square class that represents a geometric square."""
+
+
+class Square:
+    """A class that defines a square by its size and position."""
+
+    def __init__(self, size):
+        self.__size = size
+
+
+"""
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
+
+
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
+
+"""
