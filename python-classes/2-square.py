86b87c69164b4713a55340e5bfa4c034d663060e
Charlene <charlenee.huynh@gmail.com>
2026-05-04 12:23:45 +0200

    2

---
+++ python-classes/2-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""This module defines a Square class that represents a geometric square."""
+
+
+class Square:
+    """A class that defines a square by its size and position."""
+
+    def __init__(self, size = 0):
+        self.__size = size
+        if  type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+       
+my_square_1 = Square(3)
+print(type(my_square_1))
+print(my_square_1.__dict__)
+
+my_square_2 = Square()
+print(type(my_square_2))
+print(my_square_2.__dict__)
+
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_3 = Square("3")
+    print(type(my_square_3))
+    print(my_square_3.__dict__)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_4 = Square(-89)
+    print(type(my_square_4))
+    print(my_square_4.__dict__)
+except Exception as e:
+    print(e)
