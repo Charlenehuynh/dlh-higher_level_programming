b29c14f44117084584628c927ba92b250edceeba
Charlene <charlenee.huynh@gmail.com>
2026-05-04 11:00:07 +0200

    0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""This module defines a Square class that represents a geometric square."""
 
 
 class Square:
+    """A class that defines a square by its size and position."""
     pass
