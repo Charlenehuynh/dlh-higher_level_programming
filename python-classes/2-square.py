7e50383004ea57860e8254c5b97c4af71e456dff
Charlene <charlenee.huynh@gmail.com>
2026-05-04 12:24:14 +0200

    2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -11,7 +11,8 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-       
+
+'''       
 my_square_1 = Square(3)
 print(type(my_square_1))
 print(my_square_1.__dict__)
@@ -43,3 +44,4 @@ try:
     print(my_square_4.__dict__)
 except Exception as e:
     print(e)
+'''
