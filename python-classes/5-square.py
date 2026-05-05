9258090f71e6fbc1add2ac87b12b606f33c29135
Charlene <charlenee.huynh@gmail.com>
2026-05-05 16:53:36 +0200

    5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -35,6 +35,8 @@ class Square:
         self.__size = value
 
     def my_print(self):
+        if self.size == 0:
+            print("")
         for i in range(self.size):
             for j in range(self.size):
                 print("#", end="")
