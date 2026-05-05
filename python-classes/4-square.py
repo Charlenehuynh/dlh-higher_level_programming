8b315d204c10ef482ee94ec835e03b5de84c4ccb
Charlene <charlenee.huynh@gmail.com>
2026-05-05 16:12:58 +0200

    4

---
+++ python-classes/4-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""This module defines a Square class that represents a geometric square."""
+
+
+class Square:
+    """A class that defines a square by its size and position."""
+
+    def __init__(self, size=0):
+        """Initialize"""
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size**2
+
+    def size(self, value):
+        if type(self.size) is not int:
+            raise TypeError("size must be an integer")
+        if self.size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+
+"""
+my_square = Square(89)
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+my_square.size = 3
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+try:
+    my_square.size = "5 feet"
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+except Exception as e:
+    print(e)
+"""
