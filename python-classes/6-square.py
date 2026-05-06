36fb3d1057556a5916543865eced967efaa1dca6
Charlene <charlenee.huynh@gmail.com>
2026-05-06 10:53:59 +0200

    6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -21,7 +21,7 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     def area(self):
         return self.__size**2
 
@@ -40,7 +40,7 @@ class Square:
             or type(value[1]) is not int
         ):
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value 
+        self.__position = value
 
     def my_print(self):
         if self.size == 0:
@@ -54,16 +54,15 @@ class Square:
                 print("#", end="")
             print()
 
-my_square_1 = Square(3)
-my_square_1.my_print()
+# my_square_1 = Square(3)
+# my_square_1.my_print()
 
-print("--")
+# print("--")
 
-my_square_2 = Square(3, (1, 1))
-my_square_2.my_print()
+# my_square_2 = Square(3, (1, 1))
+# my_square_2.my_print()
 
-print("--")
+# print("--")
 
-my_square_3 = Square(3, (3, 0))
-my_square_3.my_print()
-
+# my_square_3 = Square(3, (3, 0))
+# my_square_3.my_print()
