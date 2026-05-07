3774c08227ff1ebe9cd98a1df8b0385f192746f0
Charlene <charlenee.huynh@gmail.com>
2026-05-07 10:24:46 +0200

    0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""This module include class rectangle that defines a rectangle"""
+
+
+class Rectangle:
+    """class define a rectangle"""
+
+    pass
+
+
+# my_rectangle = Rectangle()
+# print(type(my_rectangle))
+# print(my_rectangle.__dict__)
