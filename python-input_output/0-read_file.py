da4ae5d48b3a5b0484fe908939095f1fcc2918d5
Charlene <charlenee.huynh@gmail.com>
2026-05-07 16:00:07 +0200

    0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ def read_file(filename=""):
     """ This function is to read and print file"""
     with open(filename, "r") as f:
         result = f.read()
-        print(result)
+        print(result, end="")
 # read_file("my_file_0.txt")
