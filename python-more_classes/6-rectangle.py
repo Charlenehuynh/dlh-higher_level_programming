c4e7c263307533245c3bf4f0db8cb5eb62765a32
Charlene <charlenee.huynh@gmail.com>
2026-05-07 12:58:04 +0200

    6

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -68,10 +68,10 @@ class Rectangle:
         print("Bye rectangle...")
 
 
-my_rectangle_1 = Rectangle(2, 4)
-my_rectangle_2 = Rectangle(2, 4)
-print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
-del my_rectangle_1
-print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
-del my_rectangle_2
-print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
+# my_rectangle_1 = Rectangle(2, 4)
+# my_rectangle_2 = Rectangle(2, 4)
+# print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
+# del my_rectangle_1
+# print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
+# del my_rectangle_2
+# print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
