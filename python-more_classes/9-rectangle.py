a6e2bd762ba8369f681e3e12b9f4ab770f72568f
Charlene <charlenee.huynh@gmail.com>
2026-05-07 13:54:31 +0200

    9:

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -89,6 +89,6 @@ class Rectangle:
 
 
 # my_square = Rectangle.square(5)
-# print("Area: {} - Perimeter: 
+# print("Area: {} - Perimeter:
 # {}".format(my_square.area(), my_square.perimeter()))
 # print(my_square)
