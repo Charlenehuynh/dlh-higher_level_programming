0b2139d3fcb3f674646628ea7db572d7e928c1ec
Charlene <charlenee.huynh@gmail.com>
2026-05-07 12:44:59 +0200

    5

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+
+"""This module include class rectangle that defines a rectangle"""
+
+
+class Rectangle:
+    """class define a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    # width
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    # height
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    # Method:
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * self.__height + 2 * self.__width
+
+    def __str__(self):
+        result = ""
+        # loop over column (height)
+        for i in range(self.__height):
+            # loop over width:
+            for j in range(self.__width):
+                result += "#"
+            result += "\n"
+        new_result = result.rstrip("\n")
+        return new_result
+
+    def __repr__(self):
+        # return Rectangle(width, height)
+        return "Rectangle({}, {})".format(self.width, self.height)
+
+    def __del__(self):
+        print("Bye rectangle...")
+
+
+# my_rectangle = Rectangle(2, 4)
+# print("Area: {} - Perimeter: {}".format(my_rectangle.area(), 
+# my_rectangle.perimeter()))
+
+# del my_rectangle
+# try:
+#     print(my_rectangle)
+# except Exception as e:
+#     print("[{}] {}".format(e.__class__.__name__, e))
