43b2de91c6047d073a20510e780358d63b42806f
Charlene <charlenee.huynh@gmail.com>
2026-05-07 16:16:50 +0200

    2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -15,5 +15,6 @@ def append_write(filename="", text=""):
     return length
 
 
-# nb_characters_added = append_write("file_append.txt", "This School is so cool!\n")
+# nb_characters_added = append_write
+# ("file_append.txt", "This School is so cool!\n")
 # print(nb_characters_added)
