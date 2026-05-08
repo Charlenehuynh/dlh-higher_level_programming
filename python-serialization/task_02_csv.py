c90dd8d1521d8331593d4ef32455ae7f31953120
Charlene <charlenee.huynh@gmail.com>
2026-05-08 16:25:10 +0200

    2

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""CSV DATA to JSON FORMAT"""
+
+import csv
+import json
+
+
+def convert_csv_to_json(filename):
+    """Write json data to file
+
+    Args:
+        filename (_type_):
+    """
+    result = []
+    with open(filename, "r") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            result.append(row)
+    with open("data.json", "w") as file:
+        json.dump(result, file)
+
+
+csv_file = "data.csv"
+convert_csv_to_json(csv_file)
