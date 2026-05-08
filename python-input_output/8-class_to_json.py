7ff98fefdd903f761033f9964bf2d25c6ff1f1ec
Charlene <charlenee.huynh@gmail.com>
2026-05-08 07:22:16 +0200

    8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """This module  returns the dictionary description with simple data structure
-(list, dictionary, string, integer and boolean) 
+(list, dictionary, string, integer and boolean)
 for JSON serialization of an object:"""
 
 
