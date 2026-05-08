51797d13df5d0df0f701c3b1ff2dde9bdc7a0406
Charlene <charlenee.huynh@gmail.com>
2026-05-08 08:17:58 +0200

    10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Write a class Student that defines a student by"""
+
+
+class Student:
+    """input dict and print the attribute that match with that key dict"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        # loop through dict and compare keys with attribute name then
+        if isinstance(attrs, list):
+            result = {}
+            for i in attrs:
+                if hasattr(self, i):
+                    result[i] = getattr(self, i)
+        else:
+            result = self.__dict__
+        return result
+
+
+student_1 = Student("John", "Doe", 23)
+student_2 = Student("Bob", "Dylan", 27)
+
+j_student_1 = student_1.to_json()
+j_student_2 = student_2.to_json(["first_name", "age"])
+j_student_3 = student_2.to_json(["middle_name", "age"])
+
+print(j_student_1)
+print(j_student_2)
+print(j_student_3)
