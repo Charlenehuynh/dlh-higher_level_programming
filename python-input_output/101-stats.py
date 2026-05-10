7c5979ee050123df19dd5f840a5932df47d3ad51
Charlene <charlenee.huynh@gmail.com>
2026-05-10 19:46:48 +0200

    101

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -8,17 +8,20 @@ count = 0
 my_dict = {}
 if __name__ == "__main__":
     try:
-        """test """
         for line in sys.stdin:
-            parts = line.split()
-            count += 1
-            my_dict[parts[-2]] = my_dict.get(parts[-2], 0) + 1
-            total_size += int(parts[-1])
-            if count == 10:
-                count = 0
-                print(f"File size: {total_size}")
-                for key, value in sorted(my_dict.items()):
-                    print(f"{key}: {value}")
+            try:
+                parts = line.split()
+                # print(parts)  # temporary debug line
+                count += 1
+                my_dict[parts[-2]] = my_dict.get(parts[-2], 0) + 1
+                total_size += int(parts[-1])
+                if count == 10:
+                    count = 0
+                    print(f"File size: {total_size}")
+                    for key, value in sorted(my_dict.items()):
+                        print(f"{key}: {value}")
+            except:
+                continue
     except KeyboardInterrupt:
         print(f"File size: {total_size}")
         for key, value in sorted(my_dict.items()):
